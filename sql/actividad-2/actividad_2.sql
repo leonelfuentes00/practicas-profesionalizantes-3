f057571e4736063765902e9b319a4558c021f3f9
Fuentes Leonel <lmfuentes00@gmail.com>
2025-06-19 21:00:41 -0300

    2025/06/19 - Sube actividad 2

---
+++ sql/actividad-2/actividad_2.sql
@@ -0,0 +1,177 @@
+drop database actividad_1;
+create database actividad_1;
+
+use actividad_1;
+
+create table paises (
+	id int primary key auto_increment not null,
+	nombre varchar(40),
+    capital varchar(40),
+	idioma varchar(20),
+    superficie double,
+    poblacion int);
+
+-- 
+delimiter //
+create procedure obtenerDatosPorPais(
+	in _nombre varchar(40)
+)
+begin
+	select * from paises where nombre = _nombre;
+end;
+// delimiter;    
+
+--
+delimiter //
+create procedure crearPais(
+	in _nombre varchar(40),
+    in _capital varchar(40),
+    in _idioma varchar(20),
+    in _superficie double, 
+	in _poblacion int
+)
+begin
+	insert into paises (nombre, capital, idioma, superficie, poblacion)
+    values (_nombre, _capital, _idioma, _superficie, _poblacion);
+end;
+// delimiter;
+--
+delimiter //
+create procedure editarPais(
+	in _id int,
+    in _nombre varchar(40),
+    in _capital varchar(40),
+    in _idioma varchar(20),
+    in _superficie double, 
+	in _poblacion int
+)
+begin
+	update paises
+    set nombre = _nombre, capital = _capital, idioma = _idioma, superficie = _superficie, poblacion = _poblacion
+    where id = _id;
+end;
+// delimiter;   
+--
+delimiter //
+create procedure eliminarPais(
+	in _id int
+)
+begin
+	delete from paises where id = _id;
+end;
+// delimiter ;   
+
+--
+
+insert into paises(nombre, capital, idioma, superficie, poblacion) values
+	('argentina', 'caba', 'español', 11111111, 10101010),
+	('chile', 'santiago', 'español', 22222222, 20202020),
+	('brasil', 'brasilia', 'portugues', 33333333, 30303030),
+	('uruguay', 'montevideo', 'español', 44444444, 40404040),
+	('bolivia', 'sucre', 'español', 55555555, 50505050);
+    
+-- 
+
+call obtenerDatosPorPais('argentina');
+
+call crearPais('peru', 'lima', 'español', '66666666', '60606060');
+
+call editarPais(2, 'chile','sintiago', 'chileno','12345678','87654321');
+
+call eliminarPais(5);
+
+-- select * from paises
+
+
+-- Actividad 2
+
+create table ciudades (
+	id int primary key auto_increment not null,
+	nombre varchar(40),
+    superficie double,
+    poblacion int,
+    codigo_postal int,
+    ciudad_costera boolean,
+    id_pais varchar(50),
+    foreign key (id_pais) references paises(id)
+    );
+    
+delimiter //
+create procedure crearCiudad(
+    in _nombre varchar(40),
+    in _superficie double,
+    in _poblacion int,
+    in _codigo_postal int,
+    in _ciudad_costera boolean,
+    in _id_pais int
+)
+begin
+    insert into ciudades(nombre, superficie, poblacion, codigo_postal, ciudad_costera, id_pais)
+    values (_nombre, _superficie, _poblacion, _codigo_postal, _ciudad_costera, _id_pais);
+end;
+//
+delimiter ;
+
+delimiter //
+create procedure editarCiudad(
+    in _id int,
+    in _nombre varchar(40),
+    in _superficie double,
+    in _poblacion int,
+    in _codigo_postal int,
+    in _ciudad_costera boolean,
+    in _id_pais int
+)
+begin
+    update ciudades
+    set nombre = _nombre, superficie = _superficie, poblacion = _poblacion,
+        codigo_postal = _codigo_postal, ciudad_costera = _ciudad_costera, id_pais = _id_pais
+    where id = _id;
+end;
+//
+delimiter ;
+
+delimiter //
+create procedure eliminarCiudad(
+    in _id int
+)
+begin
+    delete from ciudades where id = _id;
+end;
+//
+delimiter ;
+
+delimiter //
+create procedure paisConCiudadMasPoblada()
+begin
+	select paises.nombre as pais from paises
+    join ciudades on ciudades.id_pais = paises.id
+    order by ciudades.poblacion desc
+    limit 1;
+    end;
+// delimiter ;
+
+delimiter //
+create procedure paisesConCiudadesCosterasConMasDeUnMillonHabitantes()
+begin 
+	select paises.nombre as pais from paises
+    join ciudades on ciudades.id_pais = paises.id
+    where ciudades.ciudad_costera = true 
+		and ciudades.poblacion > 1000000 ;
+end;
+// delimiter ;
+
+delimiter //
+create procedure ciudadConDensidadDePoblacionMasAlta()
+begin
+	select paises.nombre as pais, ciudad.nombre as ciudad,
+			(ciudad.poblacion / ciudad.superficie) as densidad
+	from paises
+    join ciudades on ciudades.id_pais = paises.id
+    order by densidad desc
+	limit 1;
+    end;
+    
+// delimiter ;
+
+
